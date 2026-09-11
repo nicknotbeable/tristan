98a56c4524fb35385a7d434b2162e658d94597fd
nicknotbeable <tristanaustria15@gmail.com>
2026-09-11 17:08:49 +0800

    Delete resource.js

--- resource.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.getElementById('themeToggle');
-
-toggleBtn.addEventListener('click', () => {
-    document.body.classList.toggle('bg-dark');
-    document.body.classList.toggle('text-light');
-});
